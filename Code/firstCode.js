55add11e85f5d707de6b30dd3dc217479baeb780
Ryeagler <Ryeagle@DreamerdeMacBook-Pro.local>
2017-08-24 17:01:47 +0800

    make directory of Code

---
+++ Code/firstCode.js
@@ -0,0 +1,21 @@
+
+var cxjObject={
+    phone:'15827336288',
+    gender:'female',
+    age:'18',
+    coffeeList:['Americano','Latte','Mocha','Cappuccino'],
+    isMemOfCCP:true
+}
+console.log(cxjObject)
+
+var lzObject={
+    lovePerson:cxjObject,
+    age:25
+}
+console.log(lzObject)
+
+var cxjPhone=cxjObject.phone
+console.log(cxjPhone)
+
+var cxjCoffee=cxjObject.coffeeList[1]
+console.log(cxjCoffee)
