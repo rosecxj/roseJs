df92287b247e65879e1957002bea5550fe03113c
Ryeagler <Ryeagle@DreamerdeMacBook-Pro.local>
2017-08-28 11:07:57 +0800

    GameOfThrones_S07E07

--- Code/forByRyeagle.js
+++ Code/forByRyeagle.js
@@ -39,3 +39,37 @@ getEvenNum();
 console.log(isEndOf4(4))
 console.log(isEndOf4(3))
 console.log(isEndOf4())
+
+function maxNum(numberA, numberB) {
+    if(numberA > numberB) {
+        return numberA;
+    }
+
+    return numberB;
+}
+
+console.log(maxNum(12, 2))
+console.log(maxNum(3, 32))
+console.log(maxNum(2, 2))
+
+
+//过滤Excel中阅读量大于3000的文章
+
+var articleList = [2323, 10233, 23230, 3000, 2999, 4300];
+
+function filter(list)
+{
+    var resultList;
+    if (Array.isArray(list)) {
+        for (var i = 0; i < list.length; i++) {
+            console.log(list[i]);
+            if (list[i] > 3000) { 
+            }
+        }                
+    }
+
+    return resultList.length ? resultList : []
+}
+
+var resultList = filter(articleList)
+console.log(resultList)
